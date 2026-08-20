18e6a8bf447402263d45daa169552d2ea400e451
Fabrik355 <mem1233213@gmail.com>
2026-08-20 15:58:25 +0300

    Add files via upload

--- solutions/library/main.py
+++ solutions/library/main.py
@@ -1,69 +1,64 @@
-hand = set() # #pfdnf ljltkf.
+hand = set() 
 book_set = set() 
 
-class User: #exist
+class User: 
     pass
-class Library: # list 
-    pass
-class Book: #find add
-    pass
-class Loan: #borrow return
-    pass
-
-def book_list():
-    global book_set
-    print(book_set, 'натъ') #
-    
-def add_book(book):
-    global book_set
-    book_set.add(book)
-    print('added') #
-    
-def borrow_book(book):
-    global hand
-    global book_set
-    if book in hand:
-        print('you already have this book')
-        return
-    elif book not in book_set:
-        print('there is no this book in library')
-        return
-    else:
-        hand.add(book)
-        print('boroved') #
-
-def return_book(book):
-    global hand
-    hand.discard(book)
-    print('returned') #
-    
-    
-def find_book(book):
-    global book_set
-    if book in book_set:
-        print('yest') #
-    else:
-        print('')
+class Library:
+    def book_list():
+        global book_set
+        print(*book_set, sep = ', ')
+    def find_book(book):
+        global book_set
+        if book in book_set:
+            print(book, 'found') 
+        else:
+            print(book, 'not found')    
+class Book:
+    def __init__(self, name_book):
+        global book_set
+        book_set.add(name_book)
+        print('Book', name_book, 'added')
+class Loan:
+    def borrow_book(book):
+        global hand
+        global book_set
+        if book in hand:
+            print('you already have this book')
+            return
+        elif book not in book_set:
+            print('there is no this book in library')
+            return
+        else:
+            hand.add(book)
+            print(book, 'successfully borrowed') 
+        
+    def return_book(book):
+        global hand
+        hand.discard(book)
+        print(book, 'successfully returned') 
 
 
 
 
 
+use = User()
+
+library1=Library
 
 while True:
     _ = input().split()
     
     
     if _[0] == 'list':
-        book_list()
+        library1.book_list()
     elif _[0] == 'add-book':
-        add_book(_[1])
+        book_ = Book(_[1])
     elif _[0] == 'borrow':
-        borrow_book(_[1])
+        Loan.borrow_book(_[1])
     elif _[0] == 'return':
-        return_book(_[1])
+        Loan.return_book(_[1])
     elif _[0] == 'find':
-        find_book(_[1])
+        library1.find_book(_[1])
     elif _[0] == 'help':
         print('nope')
     else:
